57f08aa9130f710f5fcf6bf35a6b769414282754
hasanelmestrah <hasanelmestrah@gmail.com>
2023-05-06 20:18:11 +0300

    working

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,56 @@
 import React from "react";
+//images
+import Image from "../assets/avatar.svg";
+//icons
+import { FaGithub, FaYoutube, FaDribble } from "react-icons/fa";
+//type animation
+import { TypeAnimation } from "react-type-animation";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <div className="section" id="home">
-      Banner
-    </div>
+    <section className="section" id="home">
+      <div className="container mx-auto ">
+        <div>
+          {/*text */}
+          <div>
+            <h1>
+              Hasan <span>El-Mestrah</span>
+            </h1>
+            <div className="mb-6 text-[36px] ig:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+              <span className="text-white mr-4">I am</span>
+              <TypeAnimation
+                sequence={[
+                  "Developer",
+                  2000,
+                  "Dreamer",
+                  2000,
+                  "Investor",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+              <p>
+                I'm driven by a passion for technology and the financial
+                markets, and I'm constantly seeking new challenges and
+                opportunities to innovate and excel in my work as a
+                programmer-trader
+              </p>
+            </div>
+          </div>
+          {/*image */}
+          <div>
+            <img src={Image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
