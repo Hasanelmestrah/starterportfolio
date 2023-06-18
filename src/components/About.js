44a154e983b1c7b555aa80ae3fba14f7ad789544
hasanelmestrah <hasanelmestrah@gmail.com>
2023-06-18 17:49:10 +0300

    services

--- src/components/About.js
+++ src/components/About.js
@@ -15,42 +15,51 @@ const About = () => {
   return (
     <section className="section" id="about" ref={ref}>
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
           {/*img */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+          <motion.div
+            variants={fadeIn("right", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></motion.div>
           {/*text */}
-          <div>
-            <h2>About Me</h2>
-            <h3>
+          <motion.div
+            variants={fadeIn("left", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1">
+            <h2 className="h2 text-accent">About Me</h2>
+            <h3 className="h3 mb-4">
               I'm a Freelance Front-end Dev with over 2 years of experience.{" "}
             </h3>
-            <p>
+            <p className="mb-6">
               lorem ipsum dasdas sada sda adf df dfadca asasdas fasas asfasf
               asadf adfsdvfd afsddf
             </p>
             {/* stats */}
-            <div className="flex gap-x-7 lg:gap-x-10 mb-12">
+            <div className="flex gap-y-7 lg:gap-x-10 mb-12">
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                  {inView ? <CountUp start={0} end={15} duration={3} /> : null}
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   years of <br />
-                  experience
                 </div>
               </div>
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                  {inView ? <CountUp start={0} end={5} duration={3} /> : null}{" "}
+                  k+
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   years of <br />
-                  experience
                 </div>
               </div>
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                  {inView ? <CountUp start={0} end={7} duration={3} /> : null}
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   years of <br />
@@ -58,7 +67,14 @@ const About = () => {
                 </div>
               </div>
             </div>
-          </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg"> contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                {" "}
+                my portfolio{" "}
+              </a>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,72 @@
 import React from "react";
+//icon
+import { BsArrowUpRight } from "react-icons/bs";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 
+import MyServices from "./Services";
+//services data
+const services = [
+  {
+    name: "UI/UX Design",
+    description:
+      "lorema doadasmdiasdasdjadakdodlmadoiasmdioasdnad aksdmasodmas oasmdoaidsmasodp omaopd.",
+    link: "link is here",
+  },
+  {
+    name: "Web Developmen",
+    Description:
+      "i can build to you your dream website , and you start you bussines now ",
+    link: "link is here",
+  },
+  {
+    name: "Broker",
+    Description:
+      "Start Tradnig now , with a partner top broker in lebano , stocks currency adnn crypto currency ",
+    link: "link is here",
+  },
+];
 const Services = () => {
   return (
-    <div className="section" id="services">
-      Services
-    </div>
+    <section className="section" id="services">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/*text and img*/}
+          <div className="flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12 lg:mb-0">
+            <h2 className="h2 text-accent mb-6"> What I do.</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              I'm a Freelance Front-end Dev with over 2 years of experience.
+            </h3>
+            <button className="btn btn-sm">See my work</button>
+          </div>
+          {/*services*/}
+          <div className="flex-1">
+            Description {/*services list*/}
+            <div>
+              {services.map((service, index) => {
+                //desctruction services
+                const { name, description, link } = service;
+                return (
+                  <div
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                    key={index}>
+                    <div className="max-w-[476px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {name}
+                      </h4>
+                      <p>{description}</p>
+                    </div>
+                    <div>links</div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
