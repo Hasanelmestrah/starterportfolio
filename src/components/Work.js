226dde3700343b797507e98ab96c2d680b80895f
hasanelmestrah <hasanelmestrah@gmail.com>
2023-06-23 01:17:10 +0300

    working

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,56 @@
 import React from "react";
+//motion
+import { motion } from "framer-motion";
+// variants
+import { fadeIn } from "../variants";
+// img
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
   return (
-    <div className="section" id="work">
-      Work
-    </div>
+    <section className="section" id="work">
+      <div className="container mx-auto">
+        <div>
+          <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+            {/* text */}
+            <div>
+              <h2 className="h2 leading-tight text-accent">
+                My Latest <br />
+                work.
+              </h2>
+              <p className="max-w-sm mb-16">
+                I'm driven by a passion for technology and the financial
+                markets, and I'm constantly seeking new challenges and
+                opportunities
+              </p>
+              <button className="btn btn-sm">View all Projects</button>
+            </div>
+            {/* image */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt=""
+              />
+              {/* petitee */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">UI/Ux Design </span>
+              </div>
+              {/* title */}
+              <div>
+                <span> Project Title</span>
+              </div>
+            </div>
+          </div>
+          <div>2</div>
+        </div>
+      </div>
+    </section>
   );
 };
 
